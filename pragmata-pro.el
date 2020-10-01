c19c4666025eec7c85f42b34df57c54fa82bfd23
Lennart Vogelsang <lennart@vogelsang.berlin>
2020-10-01 09:26:49 +0200

    Added explicit hook fn for font setting

--- pragmata-pro.el
+++ pragmata-pro.el
@@ -14,17 +14,22 @@
 
 (defconst pragmata-pro/font-name "PragmataPro")
 
-(load-file "./pragmatapro/emacs_snippets/pragmatapro-prettify-symbols-v0.828.el")
+(load-file (concat (file-name-directory (file-chase-links (concat (file-name-sans-extension load-file-name) ".el"))) "/pragmatapro/emacs_snippets/pragmatapro-prettify-symbols-v0.828.el"))
 
-(when (display-graphic-p)
-  (set-face-attribute 'default nil :font pragmata-pro/font-name))
+(defun pragmata-pro/set-font ()
+  "Set font. Works with direct and daemon based execution of emacs."
+    (when (display-graphic-p)
+      (set-face-attribute 'default nil :font pragmata-pro/font-name)
 
                                         ; If we started emacsclient though, and connect to an emacs server,
                                         ; some trickery is required. An emacs instance in daemon mode does not
                                         ; pickup fonts (or anything related to the graphical interface for
                                         ; that matter) until an actual GUI window is created, so we have to
                                         ; defer font loading until after the creation of a frame.
-(add-hook 'after-make-frame-functions (lambda (frame)
-                                        (select-frame frame)
-                                        (when (display-graphic-p)
-                                          (set-face-attribute 'default nil :font pragmata-pro/font-name))))
+      (add-hook 'after-make-frame-functions (lambda (frame))
+                (when (boundp 'frame)
+                  (select-frame frame))
+                (when (display-graphic-p)
+                  (set-face-attribute 'default nil :font pragmata-pro/font-name)))))
+
+(provide 'pragmata-pro)
